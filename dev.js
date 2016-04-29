a17f88419ef51ac1434f03d41971427cd3e7bc55
Michael Hartmayer <michael.hartmayer@scrollmotion.com>
2016-04-29 15:36:04 -0700

    tweaks

--- dev.js
+++ dev.js
@@ -5,28 +5,38 @@ const spawn  = cp.spawn;
 
 const log = m => console.log('*** '.green.bold, m);
 
+// candy
 console.log('\n---------------- ---------------- ----------------');
 
+// helper function to log stuff
 log('-[ E-1986 ]-'.bold.yellow);
 
+// start webpack
 log('Starting Webpack'.bold.white);
 const webpack = spawn('webpack', ['--dev'], { stdio: 'inherit' });
 
-log('Starting Server [port: 9123]'.bold.white);
+// start server
+log('Starting Server [port: 9815]'.bold.white);
 const server = spawn('node', ['server.js'], { stdio: 'inherit' });
 
+// start browser
 log('Opening in Browser'.bold.white + ' http://localhost:9815'.bold.yellow);
 const browser = spawn('open', ['http://localhost:9815/']);
 
+// on error show error
 webpack.on('error', err => log('Unable to start Webpack.'.red));
 server.on('error',   err => log('Unable to start game Server'.red));
 browser.on('error', err => log('Unable to open http://localhost:9815'.red));
 
+// kill processes on exit
 process.on('exit', function() {
-  console.log('killing', children.length, 'child processes');
-  [webpack, server, browser].forEach(function (child) {
-    child.kill();
-  });
+    // log
+    log('Killing ' + children.length + ' Child Processes.');
+    
+    [webpack, server, browser].forEach(function (child) {
+        child.kill();
+    });
 });
 
+// candy
 console.log('---------------- ---------------- ----------------\n');
